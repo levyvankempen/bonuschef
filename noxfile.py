dadadef71c3bd1cae5dc574e7170deb4b2b3691d
LevyvanKempen <l.m.v.kempen@outlook.com>
2025-09-29 16:30:29 +0200

    add sql formatting and linting

--- noxfile.py
+++ noxfile.py
@@ -1,33 +1,54 @@
 import nox
 from nox.sessions import Session
 
-nox.options.sessions = ["lint", "mypy", "tests"]
-locations = "src", "tests", "noxfile.py"
+nox.options.sessions = [
+    "lint_python",
+    "lint_sql",
+    "format_python",
+    "format_sql",
+    "mypy",
+    "tests",
+]
+locations_python = "src", "tests", "noxfile.py"
+locations_sql = ["src/bonuschef/sql"]
 
 
 @nox.session(python=["3.12"], venv_backend="uv")
 def tests(session: Session) -> None:
     args = session.posargs or ["--cov=onboarding_ruud", "-q"]
 
-    session.install("uv")
+    session.install("ruff", "uv")
     session.run("uv", "sync", "--active", external=True)
     session.run("uv", "run", "--active", "pytest", *args, external=True)
 
 
 @nox.session(python=["3.12"], venv_backend="uv")
-def lint(session: Session) -> None:
+def lint_python(session: Session) -> None:
     """Run ruff code linter."""
-    args = session.posargs or locations
+    args = session.posargs or locations_python
     session.install("ruff", "uv")
-    session.run("uv", "sync", external=True)
+    session.run("uv", "sync")
     session.run("uv", "run", "ruff", "check", *args)
     session.run("uv", "run", "ruff", "format", "--diff", *args)
 
 
-@nox.session(python="3.12", venv_backend="uv")
-def format(session: Session) -> None:
+@nox.session(python=["3.12"], venv_backend="uv")
+def lint_sql(session: Session) -> None:
+    """Lint using SQLfluff."""
+    args = session.posargs or locations_sql
+    session.install("ruff", "uv", "sqlfluff")
+    session.run("uv", "sync")
+    session.run("uv", "run", "dbt", "deps", "--project-dir", *args)
+    session.run(
+        "uv", "run", "dbt", "parse", "--project-dir", *args, "--profiles-dir", *args
+    )
+    session.run("uv", "run", "sqlfluff", "lint", "--dialect", "postgres", *args)
+
+
+@nox.session(python=["3.12"], venv_backend="uv")
+def format_python(session: Session) -> None:
     """Run ruff code formatter."""
-    args = session.posargs or locations
+    args = session.posargs or locations_python
     session.install("ruff", "uv")
     session.run("uv", "sync")
     session.run("uv", "run", "ruff", "check", "--fix", *args)
@@ -35,8 +56,17 @@ def format(session: Session) -> None:
 
 
 @nox.session(python=["3.12"], venv_backend="uv")
+def format_sql(session: Session) -> None:
+    """Run SQLfluff fix formatter."""
+    args = session.posargs or locations_sql
+    session.install("ruff", "uv", "sqlfluff")
+    session.run("uv", "sync")
+    session.run("uv", "run", "sqlfluff", "fix", "--dialect", "postgres", *args)
+
+
+@nox.session(python=["3.12"], venv_backend="uv")
 def mypy(session):
-    args = session.posargs or locations
-    session.install("uv")
+    args = session.posargs or locations_python
+    session.install("ruff", "uv")
     session.run("uv", "sync")
     session.run("uv", "run", "mypy", *args)
--- src/bonuschef/sql/models/staging/github/stg_github__products_ah.sql
+++ src/bonuschef/sql/models/staging/github/stg_github__products_ah.sql
@@ -9,12 +9,12 @@ source AS (
 renamed AS (
 
     SELECT
-        n AS name,
-        l AS link,
+        n AS product_name,
+        l AS product_link,
         p AS price,
         s AS amount
 
     FROM source
 )
 
-select * from renamed
+SELECT * FROM renamed
